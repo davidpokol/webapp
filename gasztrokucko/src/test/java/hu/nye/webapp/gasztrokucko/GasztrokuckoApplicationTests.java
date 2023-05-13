2b22361f401df8a0135ffb0f4853f8da5c73ec06
ferencorgovan <orgovanferike@gmail.com>
2023-05-13 20:12:17 +0200

    initial commit

---
+++ gasztrokucko/src/test/java/hu/nye/webapp/gasztrokucko/GasztrokuckoApplicationTests.java
@@ -0,0 +1,13 @@
+package hu.nye.webapp.gasztrokucko;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GasztrokuckoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
